0a3525c0cd5e8256af1cc7dad7fa4c502448a3ba
oddrock <oddrock@gmail.com>
2017-11-24 17:26:46 +0800

    test

---
+++ src/main/java/com/oddrock/caj2pdf/AbbyyUtils.java
@@ -0,0 +1,69 @@
+package com.oddrock.caj2pdf;
+
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import com.oddrock.common.awt.RobotManager;
+import com.oddrock.common.windows.CmdExecutor;
+
+public class AbbyyUtils {
+	private static Logger logger = Logger.getLogger(AbbyyUtils.class);
+	
+	/**
+	 * 判断ABBYY是否已打开，并位于首页
+	 * @param robotMngr
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean isHomePage(RobotManager robotMngr) throws IOException{
+		return ScreenJudgeUtils.comparePic(robotMngr, "abbyy.mark.homepage");
+	}
+	
+	/**
+	 * 判断ABBYY是否已打开，并位于word转换任务页面
+	 * @param robotMngr
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean isWordPage(RobotManager robotMngr) throws IOException{
+		return ScreenJudgeUtils.comparePic(robotMngr, "abbyy.mark.wordpage");
+	}
+	
+	/**
+	 * 判断ABBYY进程是否启动
+	 * @return
+	 * @throws IOException
+	 */
+	public static boolean isStarted() throws IOException {
+		return CmdExecutor.getSingleInstance().isAppAlive(Prop.get("abbyy.appname"));
+	}
+	
+	/**
+	 * 打开并确保打开ABBYY
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static void close() throws IOException, InterruptedException {
+		if(isStarted()) {
+			CmdExecutor.getSingleInstance().exeCmd("taskkill /f /im \"" + Prop.get("abbyy.appname") + "\"");
+		}
+		while(isStarted()) {
+			logger.warn("等待ABBYY关闭......");
+			SoftwareControlUtils.wait(Prop.getInt("interval.waitmillis"));
+		}
+		logger.warn("确认ABBYY已关闭");
+	}
+	
+	/**
+	 * 关闭并确保关闭ABBYY
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static void open() throws IOException, InterruptedException {
+		CmdExecutor.getSingleInstance().exeCmd(Prop.get("abbyy.path"));
+		while(!isStarted()) {
+			logger.warn("等待ABBYY打开......");
+			SoftwareControlUtils.wait(Prop.getInt("interval.waitmillis"));
+		}
+		logger.warn("确认ABBYY已打开");
+	}
+}
