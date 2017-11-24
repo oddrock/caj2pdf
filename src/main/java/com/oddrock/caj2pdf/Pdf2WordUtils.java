8fefc2808cf5728213a1cd8db8bfb82a1f95ff7f
oddrock <oddrock@gmail.com>
2017-11-24 17:25:57 +0800

    test

---
+++ src/main/java/com/oddrock/caj2pdf/Pdf2WordUtils.java
@@ -0,0 +1,37 @@
+package com.oddrock.caj2pdf;
+
+import java.awt.AWTException;
+import java.io.File;
+import java.io.IOException;
+import com.oddrock.common.awt.RobotManager;
+
+/**
+ * Pdf2Word转换工具类
+ * @author qzfeng
+ *
+ */
+public class Pdf2WordUtils {
+	
+	/**
+	 * 单个pdf转word
+	 * @param pdfFilePath
+	 * @return
+	 * @throws IOException 
+	 */
+	public static File pdf2word(RobotManager robotMngr, String pdfFilePath) throws IOException {
+		File wordFile = null;
+		while(true) {
+			if(AbbyyUtils.isHomePage(robotMngr)) {
+				break;
+			}
+		}
+		
+		return wordFile;
+	}
+	
+	public static void main(String[] args) throws AWTException, IOException, InterruptedException {
+		RobotManager robotMngr = new RobotManager();
+		AbbyyUtils.open();
+		//AbbyyUtils.open();
+	}
+}
