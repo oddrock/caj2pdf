cf181efb7362e9c5a5b535ff11179d231a519b9b
oddrock <oddrock@gmail.com>
2017-11-28 09:53:53 +0800

    test

--- src/main/java/com/oddrock/caj2pdf/main/DocFormatConverter.java
+++ src/main/java/com/oddrock/caj2pdf/main/DocFormatConverter.java
@@ -383,7 +383,6 @@ public class DocFormatConverter {
 	public static void main(String[] args) throws AWTException, IOException, InterruptedException, MessagingException {
 		DocFormatConverter dfc = new DocFormatConverter();
 		//dfc.txt2mobi_test();
-		//dfc.execTransform(args);
-		TxtUtils.extractFrontPart();
+		dfc.execTransform(args);
 	}
 }
