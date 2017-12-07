5e4e8518b56f785235fee804a5833af03af9dbe1
oddrock <oddrock@gmail.com>
2017-12-07 10:44:09 +0800

    test

--- src/main/java/com/oddrock/caj2pdf/qqmail/QQFileDownloadUtils.java
+++ src/main/java/com/oddrock/caj2pdf/qqmail/QQFileDownloadUtils.java
@@ -12,83 +12,66 @@ import com.oddrock.caj2pdf.utils.Prop;
 import com.oddrock.common.awt.RobotManager;
 import com.oddrock.common.windows.ClipboardUtils;
 
-public class QQFileDownloadUtils {
-	/**
-	 * 从QQ超大附件下载页面地址下载附件
-	 * @param downPageUrl：qq超大附件下载页面地址
-	 * @param fileName：附件名
-	 * @param dstDir：目标目录
-	 * @throws InterruptedException 
-	 * @throws IOException 
-	 * @throws TransformWaitTimeoutException 
-	 */
-	public static void download(RobotManager robotMngr, File dstDir, String downPageUrl) throws IOException, InterruptedException, TransformWaitTimeoutException{		
-		Browser360Utils.open(robotMngr, downPageUrl);
+public class QQFileDownloadUtils {	
+	public static void download(RobotManager robotMngr, File dstDir, String[] downPageUrls) throws IOException, InterruptedException, TransformWaitTimeoutException{		
+		Browser360Utils.close();
+		Browser360Utils.open(robotMngr);
 		Common.waitLong();
-		while(Browser360Utils.isHuifu(robotMngr)){
-			robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.huifuclosebutton.x"), Prop.getInt("browser360.coordinate.huifuclosebutton.y"));
+		if(Browser360Utils.waitHuifu(robotMngr)){
+			// 如果出现恢复栏，将恢复栏关掉
+			Browser360Utils.waitAndCloseHuifu(robotMngr);
+		}
+		// 清空所有下载项，无论是否
+		Browser360Utils.clearAllDownloadNoMatterEnd(robotMngr);
+		for(String downPageUrl: downPageUrls){
+			Browser360Utils.open(robotMngr, downPageUrl);
+			Common.waitM();
+			Browser360Utils.waitQQFileDownloadButtonExists(robotMngr);
+			Common.waitLong();
+			// 鼠标移动到下载按钮位置
+			robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.xiazaibutton.x"), Prop.getInt("browser360.coordinate.xiazaibutton.y"));
+			Common.waitShort();
+			// 右击下载按钮
+			robotMngr.clickMouseRight();
+			Common.waitM();
+			// 菜单向下移动四个位置，到“另存为”
+			for(int i=0; i<4; i++){
+				robotMngr.pressDown();
+				Common.waitShort();
+			}
+			Common.waitShort();
+			// 选中“另存为”
+			robotMngr.pressEnter();
+			Common.waitShort();
+			// 等待新建下载任务框打开
+			Browser360Utils.waitXinjianxiazairenwuOpen(robotMngr);
+			Common.waitM();
+			// 鼠标移动到“下载到”这个输入框
+			robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.xiazaidaoinput.x"), Prop.getInt("browser360.coordinate.xiazaidaoinput.y"));
 			Common.waitShort();
 			robotMngr.clickMouseLeft();
 			Common.waitShort();
-		}
-		Common.waitLong();
-		// 鼠标移动到下载按钮位置
-		robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.xiazaibutton.x"), Prop.getInt("browser360.coordinate.xiazaibutton.y"));
-		Common.waitShort();
-		// 右击下载按钮
-		robotMngr.clickMouseRight();
-		Common.waitM();
-		// 菜单向下移动四个位置，到“另存为”
-		for(int i=0; i<4; i++){
-			robotMngr.pressDown();
+			// 选中这个输入框全部字符
+			robotMngr.pressCombinationKey(KeyEvent.VK_CONTROL, KeyEvent.VK_A);
 			Common.waitShort();
-		}
-		Common.waitShort();
-		// 选中“另存为”
-		robotMngr.pressEnter();
-		Common.waitShort();
-		// 等待新建下载任务框打开
-		Browser360Utils.waitXinjianxiazairenwuOpen(robotMngr);
-		Common.waitM();
-		// 鼠标移动到“下载到”这个输入框
-		robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.xiazaidaoinput.x"), Prop.getInt("browser360.coordinate.xiazaidaoinput.y"));
-		Common.waitShort();
-		robotMngr.clickMouseLeft();
-		Common.waitShort();
-		// 选中这个输入框全部字符
-		robotMngr.pressCombinationKey(KeyEvent.VK_CONTROL, KeyEvent.VK_A);
-		Common.waitShort();
-		dstDir.mkdirs();
-		Common.waitShort();
-		// 将生成的目标文件夹地址复制到剪贴板
-		ClipboardUtils.setSysClipboardText(dstDir.getCanonicalPath());
-		Common.waitShort();
-		// 将剪贴板里的文件路径复制到输入框
-		robotMngr.pressCombinationKey(KeyEvent.VK_CONTROL, KeyEvent.VK_V);
-		Common.waitShort();
-		// 确认下载
-		robotMngr.pressEnter();
-		Common.waitShort();
-		// 等待直到下载页面打开
-		Browser360Utils.waitXiazaiPageOpen(robotMngr);
-		Common.waitShort();
-		Browser360Utils.waitXiazaiEnd(robotMngr);
-		Common.waitShort();
-		// 鼠标移动到“清空已下载”这个button
-		robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.qingkongyixiazaibutton.x"), Prop.getInt("browser360.coordinate.qingkongyixiazaibutton.y"));
-		Common.waitShort();
-		robotMngr.clickMouseLeft();
-		Common.waitLong();
-		// 如果“删除下载任务”窗口被打开
-		if(Browser360Utils.isShanchuxiazairenwuPageOpen(robotMngr)){
-			// 取消掉“同时删除文件”这个选项
-			Browser360Utils.offTongshishanchuwenjianCheckbox(robotMngr);
+			dstDir.mkdirs();
 			Common.waitShort();
-			// 确认删除
+			// 将生成的目标文件夹地址复制到剪贴板
+			ClipboardUtils.setSysClipboardText(dstDir.getCanonicalPath());
+			Common.waitShort();
+			// 将剪贴板里的文件路径复制到输入框
+			robotMngr.pressCombinationKey(KeyEvent.VK_CONTROL, KeyEvent.VK_V);
+			Common.waitShort();
+			// 确认下载
 			robotMngr.pressEnter();
 			Common.waitShort();
+			// 等待直到下载页面打开
+			Browser360Utils.waitXiazaiPageOpen(robotMngr);
+			Common.waitLong();
+			
 		}
-		Common.waitM();	
+		Browser360Utils.waitAndClearDownloadEnd(robotMngr);
+		Browser360Utils.close();
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException, AWTException, TransformWaitTimeoutException {
@@ -97,10 +80,7 @@ public class QQFileDownloadUtils {
 		String url1 = "http://mail.qq.com/cgi-bin/ftnExs_download?k=2b6161633e4e7ac476ba577b1e610017075553065900015b485903025d4c060e03054c025b575015555700050103000d5256585238783208544cdea58c921f8c96ab9cdde5dacb8cc44c504d481146406517&t=exs_ftn_download&code=eaac8a28";
 		String url2 = "http://mail.qq.com/cgi-bin/ftnExs_download?k=2e36613161628aca76ed5729176157180452000957075d5248030703084c510604024c080950531a51530706540556025d5352003177658ecefddf8cd8a8b91ad89cdcc4e184d5d14b461145496113&t=exs_ftn_download&code=e6a11ae7";
 		String url3 = "http://mail.qq.com/cgi-bin/ftnExs_download?k=6a3333384d80fdc52be80520133101160a03500054000758150a050f511c075d5a0a1e00020652140955560802530a5d0c0a5109352c330b080204e8f0fe91898afb98f4d0fe86fcd8e2868af1f0fc17484347403547&t=exs_ftn_download&code=83385139";
-		Browser360Utils.closeOpenClearAllDownloadClose(robotMngr);
-		download(robotMngr, dstDir, url1);
-		download(robotMngr, dstDir, url2);
-		download(robotMngr, dstDir, url3);
-		Browser360Utils.close();
+		String[] urlArr = new String[]{url1,url2,url3};
+		download(robotMngr, dstDir, urlArr);
 	}
 }
--- src/main/java/com/oddrock/caj2pdf/utils/Browser360Utils.java
+++ src/main/java/com/oddrock/caj2pdf/utils/Browser360Utils.java
@@ -16,37 +16,75 @@ import com.oddrock.common.windows.CmdExecutor;
 public class Browser360Utils {
 	private static Logger logger = Logger.getLogger(Browser360Utils.class);
 	
-	public static boolean isHuifu(RobotManager robotMngr) throws IOException{
+	// 监测QQ下载按钮是否存在
+	public static boolean isQQFileDownloadButtonExists(RobotManager robotMngr) throws IOException{
 		boolean flag = false;
-		BufferedImage image = robotMngr.createScreenCapture(Prop.getInt("browser360.mark.huifu.x")
-				,Prop.getInt("browser360.mark.huifu.y")
-				,Prop.getInt("browser360.mark.huifu.width")
-				,Prop.getInt("browser360.mark.huifu.height"));
-		if(PictureComparator.compare(image, BufferedImageUtils.read(Prop.get("browser360.mark.huifu.picfilepath")))>=0.9){
+		BufferedImage image = robotMngr.createScreenCapture(Prop.getInt("browser360.mark.qqfiledownloadbutton.x")
+				,Prop.getInt("browser360.mark.qqfiledownloadbutton.y")
+				,Prop.getInt("browser360.mark.qqfiledownloadbutton.width")
+				,Prop.getInt("browser360.mark.qqfiledownloadbutton.height"));
+		if(PictureComparator.compare(image, BufferedImageUtils.read(Prop.get("browser360.mark.qqfiledownloadbutton.picfilepath")))>=0.9){
 			flag = true;
 		}
 		return flag;
 	}
 	
-	public static void closeOpenClearAllDownloadClose(RobotManager robotMngr) throws IOException, TransformWaitTimeoutException, InterruptedException{
-		Browser360Utils.close();
-		Browser360Utils.open(robotMngr);
-		Browser360Utils.clearAllDownload(robotMngr);
-		Browser360Utils.close();
+	// 等待直到QQ下载按钮存在
+	public static void waitQQFileDownloadButtonExists(RobotManager robotMngr) throws IOException, InterruptedException, TransformWaitTimeoutException{
+		Timer timer = new Timer().start();
+		while(isQQFileDownloadButtonExists(robotMngr)){
+			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.long")) {
+				logger.warn("等待QQ下载按钮出现时间过长，已达到："+timer.getSpentTimeMillis()/1000L+"秒");
+				throw new TransformWaitTimeoutException();
+			}
+			Common.waitM();
+		}
+		logger.warn("确认QQ下载按钮已出现");
 	}
 	
-	// 清空已下载
-	public static void clearAllDownload(RobotManager robotMngr) throws IOException, TransformWaitTimeoutException, InterruptedException{
-		if(isOpen(robotMngr)){
-			/*robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.menu.x"), Prop.getInt("browser360.coordinate.menu.y"));
-			Common.waitM();
+	// 等待直到关闭恢复栏
+	public static void waitAndCloseHuifu(RobotManager robotMngr) throws IOException, InterruptedException, TransformWaitTimeoutException{
+		Timer timer = new Timer().start();
+		while(isHuifu(robotMngr)){
+			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.middle")) {
+				logger.warn("等待恢复栏去掉时间过长，已达到："+timer.getSpentTimeMillis()/1000L+"秒");
+				throw new TransformWaitTimeoutException();
+			}
+			robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.huifuclosebutton.x"), Prop.getInt("browser360.coordinate.huifuclosebutton.y"));
+			Common.waitShort();
 			robotMngr.clickMouseLeft();
-			Common.waitM();
-			waitMenuOpen(robotMngr);*/
+			Common.waitShort();
+		}
+		logger.warn("确认已将恢复栏去掉");
+	}
+	
+	// 等待看是否出现恢复栏，最多等待2个long等待时间
+	public static boolean waitHuifu(RobotManager robotMngr) throws IOException, InterruptedException{
+		for(int i=0; i<3; i++){
+			if(isHuifu(robotMngr)){
+				return true;
+			}
 			Common.waitLong();
-			// 打开“下载页面”
-			robotMngr.pressCombinationKey(KeyEvent.VK_CONTROL, KeyEvent.VK_J);
-			waitXiazaiPageOpen(robotMngr);
+		}
+		return false;
+	}
+	
+	// 监测下载页面是否全部下载完成
+	public static boolean isNoDownload(RobotManager robotMngr) throws IOException{
+		boolean flag = false;
+		BufferedImage image = robotMngr.createScreenCapture(Prop.getInt("browser360.mark.nodownload.x")
+				,Prop.getInt("browser360.mark.nodownload.y")
+				,Prop.getInt("browser360.mark.nodownload.width")
+				,Prop.getInt("browser360.mark.nodownload.height"));
+		if(PictureComparator.compare(image, BufferedImageUtils.read(Prop.get("browser360.mark.nodownload.picfilepath")))>=0.9){
+			flag = true;
+		}
+		return flag;
+	}
+	
+	// 不断清理直到所有任务等待直到下载完成
+	public static void waitAndClearDownloadEnd(RobotManager robotMngr) throws IOException, InterruptedException{
+		while(!isNoDownload(robotMngr)){
 			// 鼠标移动到“清空已下载”这个button
 			robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.qingkongyixiazaibutton.x"), Prop.getInt("browser360.coordinate.qingkongyixiazaibutton.y"));
 			Common.waitShort();
@@ -61,35 +99,50 @@ public class Browser360Utils {
 				robotMngr.pressEnter();
 				Common.waitShort();
 			}
-			Common.waitShort();
+			Common.waitM();
 		}
 	}
 	
-	// 测试“菜单”是否被打开
-	public static boolean isMenuOpen(RobotManager robotMngr) throws IOException{
+	// 监测恢复栏是否存在
+	public static boolean isHuifu(RobotManager robotMngr) throws IOException{
 		boolean flag = false;
-		BufferedImage image = robotMngr.createScreenCapture(Prop.getInt("browser360.mark.menuopen.x")
-				,Prop.getInt("browser360.mark.menuopen.y")
-				,Prop.getInt("browser360.mark.menuopen.width")
-				,Prop.getInt("browser360.mark.menuopen.height"));
-		if(PictureComparator.compare(image, BufferedImageUtils.read(Prop.get("browser360.mark.menuopen.picfilepath")))>=0.9){
+		BufferedImage image = robotMngr.createScreenCapture(Prop.getInt("browser360.mark.huifu.x")
+				,Prop.getInt("browser360.mark.huifu.y")
+				,Prop.getInt("browser360.mark.huifu.width")
+				,Prop.getInt("browser360.mark.huifu.height"));
+		if(PictureComparator.compare(image, BufferedImageUtils.read(Prop.get("browser360.mark.huifu.picfilepath")))>=0.9){
 			flag = true;
 		}
 		return flag;
 	}
 	
-	// 等待直到菜单打开
-	public static void waitMenuOpen(RobotManager robotMngr) throws IOException, InterruptedException, TransformWaitTimeoutException {
-		Timer timer = new Timer().start();
-		while(!isMenuOpen(robotMngr)) {
-			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.browser360.waitmenuopen")) {
-				logger.warn("等待菜单打开时间过长，已达到："+timer.getSpentTimeMillis()/1000L+"秒");
-				throw new TransformWaitTimeoutException();
+	// 清空所有下载项，不管有没有完成
+	public static void clearAllDownloadNoMatterEnd(RobotManager robotMngr) throws IOException, TransformWaitTimeoutException, InterruptedException{
+		// 打开“下载页面”
+		robotMngr.pressCombinationKey(KeyEvent.VK_CONTROL, KeyEvent.VK_J);
+		Common.waitShort();
+		waitXiazaiPageOpen(robotMngr);
+		// 如果存在下载项
+		while(!isNoDownload(robotMngr)){
+			// 鼠标移动到第一个下载任务的“删除”button
+			robotMngr.moveMouseTo(Prop.getInt("browser360.coordinate.delfirstdownload.x"), Prop.getInt("browser360.coordinate.delfirstdownload.y"));
+			Common.waitM();
+			// 点击删除
+			robotMngr.clickMouseLeft();
+			Common.waitM();
+			// 等待下载删除页面打开
+			waitShanchuxiazairenwuPageOpen(robotMngr);
+			// 如果“删除下载任务”窗口被打开
+			if(Browser360Utils.isShanchuxiazairenwuPageOpen(robotMngr)){
+				// 取消掉“同时删除文件”这个选项
+				Browser360Utils.offTongshishanchuwenjianCheckbox(robotMngr);
+				Common.waitShort();
+				// 确认删除
+				robotMngr.pressEnter();
+				Common.waitShort();
 			}
-			logger.warn("等待菜单打开");
-			Common.waitShort();
+			Common.waitM();
 		}
-		logger.warn("确认菜单已打开");
 	}
 	
 	// 测试“同时删除文件”这个checkbox是否被选中了
@@ -130,45 +183,17 @@ public class Browser360Utils {
 		return flag;
 	}
 	
-	// 等待直到下载任务完成
+	// 等待直到下载删除窗口打开
 	public static void waitShanchuxiazairenwuPageOpen(RobotManager robotMngr) throws IOException, InterruptedException, TransformWaitTimeoutException {
 		Timer timer = new Timer().start();
 		while(!isShanchuxiazairenwuPageOpen(robotMngr)) {
-			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.browser360.waitxiaizaiend")) {
-				logger.warn("等待删除下载任务窗口过长，已达到："+timer.getSpentTimeMillis()/1000L+"秒");
-				throw new TransformWaitTimeoutException();
+			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.short")) {
+				break;
 			}
 			logger.warn("等待删除下载任务窗口打开");
 			Common.waitShort();
 		}
 		logger.warn("确认删除下载任务窗口打开");
-	}
-	
-	// 测试下载任务是否完成
-	public static boolean isXiazaiEnd(RobotManager robotMngr) throws IOException{
-		boolean flag = false;
-		BufferedImage image = robotMngr.createScreenCapture(Prop.getInt("browser360.mark.xiazaiend.x")
-				,Prop.getInt("browser360.mark.xiazaiend.y")
-				,Prop.getInt("browser360.mark.xiazaiend.width")
-				,Prop.getInt("browser360.mark.xiazaiend.height"));
-		if(PictureComparator.compare(image, BufferedImageUtils.read(Prop.get("browser360.mark.xiazaiend.picfilepath")))>=0.9){
-			flag = true;
-		}
-		return flag;
-	}
-	
-	// 等待直到下载任务完成
-	public static void waitXiazaiEnd(RobotManager robotMngr) throws IOException, InterruptedException, TransformWaitTimeoutException {
-		Timer timer = new Timer().start();
-		while(!isXiazaiEnd(robotMngr)) {
-			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.browser360.waitxiaizaiend")) {
-				logger.warn("等待下载任务完成过长，已达到："+timer.getSpentTimeMillis()/1000L+"秒");
-				throw new TransformWaitTimeoutException();
-			}
-			logger.warn("等待下载任务完成");
-			Common.waitShort();
-		}
-		logger.warn("确认下载任务已完成");
 	}
 	
 	// 测试新建下载页面是否打开
@@ -188,7 +213,7 @@ public class Browser360Utils {
 	public static void waitXiazaiPageOpen(RobotManager robotMngr) throws IOException, InterruptedException, TransformWaitTimeoutException {
 		Timer timer = new Timer().start();
 		while(!isXiazaiPageOpen(robotMngr)) {
-			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.browser360.waitxiaizaipageopen")) {
+			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.short")) {
 				logger.warn("等待下载页面打开时间过长，已达到："+timer.getSpentTimeMillis()/1000L+"秒");
 				throw new TransformWaitTimeoutException();
 			}
@@ -215,7 +240,7 @@ public class Browser360Utils {
 	public static void waitXinjianxiazairenwuOpen(RobotManager robotMngr) throws IOException, InterruptedException, TransformWaitTimeoutException {
 		Timer timer = new Timer().start();
 		while(!isXinjianxiazairenwuOpen(robotMngr)) {
-			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.browser360.waitxinjianxiazairenwuopen")) {
+			if(timer.getSpentTimeMillis()>TimeoutUtils.getTimeout("timeout.short")) {
 				logger.warn("等待新建下载任务框打开时间过长，已达到："+timer.getSpentTimeMillis()/1000L+"秒");
 				throw new TransformWaitTimeoutException();
 			}
@@ -258,24 +283,17 @@ public class Browser360Utils {
 	
 	// 打开360浏览器
 	public static void open(RobotManager robotMngr) throws IOException, InterruptedException {
-		logger.warn("启动360浏览器...");
-		CmdExecutor.getSingleInstance().exeCmd(Prop.get("browser360.path"));
-		while(!isStart()) {
-			logger.warn("等待360浏览器启动......");
-			Common.waitShort();
-		}
-		logger.warn("确认360浏览器已启动");
-		while(!isOpen(robotMngr)) {
-			logger.warn("等待360浏览器打开");
-			Common.waitShort();
-		}
-		logger.warn("确认360浏览器已打开");
+		open(robotMngr, null);
 	}
 	
 	// 用360浏览器打开一个网址
 	public static void open(RobotManager robotMngr, String url) throws IOException, InterruptedException {
 		logger.warn("启动360浏览器...");
-		CmdExecutor.getSingleInstance().exeCmd(Prop.get("browser360.path")+ " \"" + url + "\"");
+		if(url==null || url.trim().length()==0){
+			CmdExecutor.getSingleInstance().exeCmd(Prop.get("browser360.path"));
+		}else{
+			CmdExecutor.getSingleInstance().exeCmd(Prop.get("browser360.path")+ " \"" + url + "\"");
+		}
 		while(!isStart()) {
 			logger.warn("等待360浏览器启动......");
 			Common.waitShort();
@@ -293,7 +311,7 @@ public class Browser360Utils {
 		RobotManager r = new RobotManager();
 		//String url = "https://mail.qq.com/cgi-bin/ftnExs_download?k=2c626134314929cb71b9572c1166531855565951555758544f5202560f4b550e56074c560e52591a0153075551560503005a5456377f6107534fdef283954c8391a89c8aeadd9883c34f501a4716154f6214&t=exs_ftn_download&code=bba47fa7";
 		open(r);
-		clearAllDownload(r);
+		clearAllDownloadNoMatterEnd(r);
 	}
 
 }
