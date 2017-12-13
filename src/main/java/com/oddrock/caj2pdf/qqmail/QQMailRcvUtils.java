7b03d54cd5a7ff3b83e72421f9f903fdc1919bfb
oddrock <oddrock@gmail.com>
2017-12-14 03:21:27 +0800

    test

--- src/main/java/com/oddrock/caj2pdf/qqmail/QQMailRcvUtils.java
+++ src/main/java/com/oddrock/caj2pdf/qqmail/QQMailRcvUtils.java
@@ -4,46 +4,45 @@ import java.awt.AWTException;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import org.apache.log4j.Logger;
-
 import com.oddrock.caj2pdf.exception.TransformWaitTimeoutException;
 import com.oddrock.caj2pdf.utils.Prop;
 import com.oddrock.common.awt.RobotManager;
-import com.oddrock.common.mail.FromMailAttachDownloadDirGenerator;
+import com.oddrock.common.mail.AttachDownloadDirGenerator;
 import com.oddrock.common.mail.GeneralAttachDownloadDirGenerator;
-import com.oddrock.common.mail.ImapMailRcvr;
 import com.oddrock.common.mail.MailRecv;
 import com.oddrock.common.mail.PopMailRcvr;
+import com.oddrock.common.mail.PopMailReadRecordManager;
 import com.oddrock.common.mail.qqmail.QQFileDownloadPage;
 import com.oddrock.common.mail.qqmail.QQFileDownloader;
 
 public class QQMailRcvUtils {
 	private static Logger logger = Logger.getLogger(QQMailRcvUtils.class);
 	
-	public static File rcvMailByImap(String imapServer, String account, String passwd, 
-			String folderName, boolean readwriteFlag, boolean downloadAttachToLocal, String localBaseDirPath) throws Exception{
-		ImapMailRcvr imr = new ImapMailRcvr();
-		List<MailRecv> mails = imr.rcvMail(imapServer, account, passwd, folderName, readwriteFlag, downloadAttachToLocal, localBaseDirPath, new GeneralAttachDownloadDirGenerator());
-		File dstDir = null;
-		for(MailRecv mail : mails){
-			dstDir = downloadQQFileInMail(mail, localBaseDirPath);
-		}
-		return dstDir;
-	}
-	
 	public static File rcvMail(String server, String account, String passwd, 
 			String folderName, boolean readwriteFlag, boolean downloadAttachToLocal, String localBaseDirPath) throws Exception{
-		PopMailRcvr imr = new PopMailRcvr();
-		List<MailRecv> mails = imr.rcvMail(server, account, passwd, folderName, readwriteFlag, downloadAttachToLocal, localBaseDirPath, new GeneralAttachDownloadDirGenerator());
+		List<MailRecv> mails = null;
 		File dstDir = null;
-		for(MailRecv mail : mails){
-			dstDir = downloadQQFileInMail(mail, localBaseDirPath);
+		try {
+			PopMailRcvr imr = new PopMailRcvr();
+			AttachDownloadDirGenerator generator = new GeneralAttachDownloadDirGenerator();
+			mails = imr.rcvMail(server, account, passwd, folderName, readwriteFlag, downloadAttachToLocal, localBaseDirPath, generator);
+			for(MailRecv mail : mails){
+				dstDir = downloadQQFileInMail(mail, localBaseDirPath, generator);
+			}
+		}catch(Exception e) {
+			// 如果出现异常，则回滚已记录的邮件UID，便于重新下载。
+			if(mails!=null) {
+				for(MailRecv mail: mails) {
+					PopMailReadRecordManager.instance.setUnRead(account, mail.getUID());
+				}
+			}
+			throw e;
 		}
 		return dstDir;
 	}
 
-	private static File downloadQQFileInMail(MailRecv mail, String localBaseDirPath) throws TransformWaitTimeoutException, IOException, InterruptedException, AWTException {
+	private static File downloadQQFileInMail(MailRecv mail, String localBaseDirPath, AttachDownloadDirGenerator generator) throws TransformWaitTimeoutException, IOException, InterruptedException, AWTException {
 		List<QQFileDownloadPage> list = QQFileDownloader.parseQQFileDownloadPageFromQQMail(mail.getPlainContent());
 		if(list.size()==0) return null;
 		logger.warn("开始下载【"+mail.getFrom()+"】主题为【"+mail.getSubject()+"】的邮件中的QQ中转站文件...");
@@ -53,7 +52,7 @@ public class QQMailRcvUtils {
 			urlArr[i] = page.getPageUrl();
 			i++;
 		}
-		File localDir = new FromMailAttachDownloadDirGenerator().generateDir(new File(localBaseDirPath), mail);
+		File localDir = generator.generateDir(new File(localBaseDirPath), mail);
 		localDir.mkdirs();
 		QQFileDownloadUtils.download(new RobotManager(), localDir, urlArr);
 		logger.warn("结束下载【"+mail.getFrom()+"】主题为【"+mail.getSubject()+"】的邮件中的QQ中转站文件");
