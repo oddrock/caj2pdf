8e2bcbde34a5eb38574682ad59349b75e7711f7b
oddrock <oddrock@gmail.com>
2018-04-20 09:49:08 +0800

    upgrade

---
+++ src/main/java/com/oddrock/caj2pdf/constant/TransformType.java
@@ -0,0 +1,12 @@
+package com.oddrock.caj2pdf.constant;
+
+public enum TransformType {
+	caj2word,
+	caj2pdf,
+	pdf2word,
+	pdf2mobi_bycalibre,
+	txt2mobi,
+	img2word,
+	pdf2epub,
+	pdf2mobi_byabbyy,
+}
